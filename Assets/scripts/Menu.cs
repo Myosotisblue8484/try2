fde51b4287da8b6387cbf4cabc7ffbf2443439f8
Tim L <timlange2004@gmail.com>
2026-01-05 15:01:55 +0100

    FUCK THAT CODE

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void PlayGame() 
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
