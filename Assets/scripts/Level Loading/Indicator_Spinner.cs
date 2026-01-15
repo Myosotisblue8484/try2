546e6e97a8269a5f2b65b1d0e40df5c73328c125
Tim L <timlange2004@gmail.com>
2026-01-15 13:13:16 +0100

    Restructuring

---
+++ Assets/scripts/Level Loading/Indicator_Spinner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Indicator_Spinner : MonoBehaviour
+{
+    public float SpinSpeed = 5;
+    public Vector2 SpinAxis;
+    
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
